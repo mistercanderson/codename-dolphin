fe94c535bffaca5d1b081f047595b9d3652b72ab
Chris Anderson <mistercanderson@gmail.com>
2021-07-21 10:38:07 -0600

    Update tests

--- cypress/integration/codeName_Dolphin/Keyboard_spec.js
+++ cypress/integration/codeName_Dolphin/Keyboard_spec.js
@@ -1,57 +1,62 @@
-
 describe('Keyboard', () => {
   beforeEach(() => {
-    cy.visit('/synth')
-  })
+    cy.visit('/synth');
+  });
 
   it('Should be at /synth path', () => {
-    cy.url().should('include', '/synth')
-  })
+    cy.url().should('include', '/synth');
+  });
 
   it('Should respond to keyboard press', () => {
-    cy.get('[note=F4]')
-    .should('have.css', 'background-color', 'rgba(255, 255, 255, 0.847)')
-    .get('[note=F4]')
-    .trigger('keydown', { keyCode: 70})
-    .should('not.have.css', 'background-color', 'rgba(255, 255, 255, 0.847)' )
-  })
+    cy.get('[note=F4]').as('key')
+    cy.get('@key')
+      .trigger('keydown', { keyCode: 70 })
+      .wait(20)
+      .invoke('attr', 'style')
+      .as('color2');
+    cy.wait(1000);
+    cy.get('@color2')
+    .then((color2) => {
+      cy.get('@key').invoke('attr', 'style').should('not.eq', color2);
+    });
+  });
 
   it('Should respond to a click or touch', () => {
+    cy.get('[note="D#4"]').should(
+      'have.css',
+      'background-color',
+      'rgb(50, 58, 158)'
+    );
     cy.get('[note="D#4"]')
-    .should('have.css', 'background-color', 'rgb(50, 58, 158)')
-    cy.get('[note="D#4"]').click()
-    .should('not.eq', 'background-color', 'rgb(50, 58, 158)' )
-  })
-  
+      .click()
+      .should('not.eq', 'background-color', 'rgb(50, 58, 158)');
+  });
+
   it('Should have a "Key Help" toggle', () => {
-    cy.viewport('macbook-15')
-    .get('p')
-    .contains('Key Help')
-  })
+    cy.viewport('macbook-15').get('p').contains('Key Help');
+  });
 
   it('Should have keys with visible note values', () => {
-    cy.viewport('macbook-15')
-    .get('[note="C4"]')
-    .contains('A')
-  })
+    cy.viewport('macbook-15').get('[note="C4"]').contains('A');
+  });
 
   it('Should toggle key values on and off when clicked', () => {
-    cy.viewport(1920, 975) 
-    cy.get('[value=""]:button').click()
-    cy.get('[note="C4"]')
-    .should('have.text', '')
-  })
+    cy.viewport(1920, 975);
+    cy.get('[value=""]:button').click();
+    cy.get('[note="C4"]').should('have.text', '');
+  });
 
-  it("Should not respond to key presses not assigned to synth object", () => {
+  it('Should not respond to key presses not assigned to synth object', () => {
     cy.get('.keyboard')
-    .trigger('keydown', {keycode: 90})
-    .trigger('keydown', {keycode: 88})
-    .trigger('keydown', {keycode: 67})
-    cy.get('.keyboard'). within(() => {
-      cy.get('.key')
-      .should('have.css', 'background-color', 'rgba(255, 255, 255, 0.847)')
-    })
-  })
-
-})
- 
+      .trigger('keydown', { keycode: 90 })
+      .trigger('keydown', { keycode: 88 })
+      .trigger('keydown', { keycode: 67 });
+    cy.get('.keyboard').within(() => {
+      cy.get('.key').should(
+        'have.css',
+        'background-color',
+        'rgba(255, 255, 255, 0.847)'
+      );
+    });
+  });
+});
