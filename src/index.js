ae6cfcb047e4e22744e668a5010b8880862ba4eb
Chris Anderson <mistercanderson@gmail.com>
2021-07-20 20:43:44 -0600

    Update manifest + icons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-const router = <BrowserRouter> <App /> </BrowserRouter>;
+ReactDOM.render(
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <App />
+    {/* </React.StrictMode>  */}
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(router, document.getElementById('root'));
+serviceWorkerRegistration.register();
+
+reportWebVitals();
