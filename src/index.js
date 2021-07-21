cffe740cd21148b2ba154d327ac49e79ce53f8e7
Chris Anderson <mistercanderson@gmail.com>
2021-07-21 00:24:41 -0600

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-//   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>, 
-//   </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 // Check that service workers are supported
 
